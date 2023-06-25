f7cfa8e35f4ab48df18e7f529ffb28b57a944ab8
abhayg76 <101787792+abhayg76@users.noreply.github.com>
2023-06-25 10:08:07 +0530

    uploaded files

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const dotenv= require('dotenv');
+dotenv.config();
+const db = process.env.MONGO_URL;
+
+// Connection to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
